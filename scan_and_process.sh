8b065bc0b061d74b61a6d6861d1519075b5cb4de
Linus Wallgren <linus@noomi.com>
2019-03-21 14:36:21 +0100

    Support sytsemd version 235 and older

--- scan_and_process.sh
+++ scan_and_process.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd "/var/lib/prometheus-sshwifi" || exit 2
 workdir="${RUNTIME_DIRECTORY:-/run/prometheus-sshwifi}/$(date --iso-8601=s)"
 mkdir -p metrics
 "$(dirname "$0")/scan.sh" "$workdir"
