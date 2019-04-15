3a6cde0019a5f5d7270f886d6ab5ea30090bd750
Linus Wallgren <linus@noomi.com>
2019-04-15 14:34:50 +0200

    Fix many linting issues
    Mostly related to non-quoted words

--- process.sh
+++ process.sh
@@ -5,17 +5,17 @@ get_mac() {
 	grep "$ip" /var/lib/promscan/all | awk '{ print $3 }'
 }
 
-for i in $1/*; do
+for i in "$1"/*; do
 	ssid=$(awk '/SSID/ { print $2 }' < "$i")
 	bssid=$(awk '/Connected to/ { print $3 }' < "$i")
 	freq=$(awk '/freq/ { print $2 }' < "$i")
 	strength=$(awk '/signal/ { print $2 }' < "$i")
-	filename="$(basename $i)"
-	if [ -z "$ssid" -o -z "$strength" ]; then
+	filename="$(basename "$i")"
+	if [ -z "$ssid" ] || [ -z "$strength" ]; then
 		continue
 	fi
 	ip="${filename%.log}"
 	ip="${ip#iw_wlan0_link.}"
 	mac=$(get_mac "$ip")
-	printf 'associated_ap{mac="%s",ssid="%s",bssid="%s",freq="%s"} %s\n' $mac $ssid $bssid $freq $strength
+	printf 'associated_ap{mac="%s",ssid="%s",bssid="%s",freq="%s"} %s\n' "$mac" "$ssid" "$bssid" "$freq" "$strength"
 done
